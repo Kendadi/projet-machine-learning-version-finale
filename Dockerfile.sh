3f8f26d27714a893d951e3b9dcbd8f89ecd870e5
kendadi mohammed <45831243+Kendadi@users.noreply.github.com>
2023-01-28 15:56:12 -0500

    Create Dockerfile.sh

---
+++ Dockerfile.sh
@@ -0,0 +1,62 @@
+apt-get update update
+apt-get install -y python3-pip
+
+#pip3 install pip
+
+
+
+
+pip3 install matplotlib
+pip3 install absl-py==0.11.0 --no-cache-dir
+pip3 install astunparse==1.6.3 --no-cache-dir
+pip3 install cached-property==1.5.2 --no-cache-dir
+pip3 install cachetools==4.1.1 --no-cache-dir
+pip3 install certifi==2020.12.5 --no-cache-dir
+pip3 install chardet==3.0.4 --no-cache-dir
+pip3 install gast==0.3.3 --no-cache-dir
+pip3 install google-auth==1.23.0 --no-cache-dir
+pip3 install google-auth-oauthlib==0.4.2 --no-cache-dir
+pip3 install google-pasta==0.2.0 --no-cache-dir
+pip3 install grpcio==1.32.0 --no-cache-dir
+pip3 install h5py==2.10.0 --no-cache-dir
+pip3 install idna==2.10 --no-cache-dir
+pip3 install imbalanced-learn==0.7.0 --no-cache-dir
+pip3 install imblearn==0.0 --no-cache-dir
+pip3 install importlib-metadata==3.1.1 --no-cache-dir
+pip3 install joblib==0.17.0 --no-cache-dir
+pip3 install Keras-Preprocessing==1.1.2 --no-cache-dir
+pip3 install Markdown==3.3.3 --no-cache-dir
+pip3 install numpy==1.19.2 --no-cache-dir
+pip3 install oauthlib==3.1.0 --no-cache-dir
+pip3 install opt-einsum==3.3.0 --no-cache-dir
+pip3 install pandas==1.1.4 --no-cache-dir
+pip3 install protobuf==3.14.0 --no-cache-dir
+pip3 install pyasn1==0.4.8 --no-cache-dir
+pip3 install pyasn1-modules==0.2.8 --no-cache-dir
+pip3 install python-dateutil==2.8.1 --no-cache-dir
+pip3 install pytz==2020.4 --no-cache-dir
+pip3 install requests==2.25.0 --no-cache-dir
+pip3 install requests-oauthlib==1.3.0 --no-cache-dir
+pip3 install rsa==4.6 --no-cache-dir
+pip3 install scikit-learn==0.23.2 --no-cache-dir
+pip3 install scipy==1.5.4 --no-cache-dir
+pip3 install six==1.15.0 --no-cache-dir
+pip3 install tensorboard==2.4.0 --no-cache-dir
+pip3 install tensorboard-plugin-wit==1.7.0 --no-cache-dir
+pip3 install tensorflow==2.4.1 --no-cache-dir
+pip3 install tensorflow-estimator==2.4.10 --no-cache-dir
+pip3 install termcolor==1.1.0 --no-cache-dir
+pip3 install threadpoolctl==2.1.0 --no-cache-dir
+pip3 install tqdm==4.54.1 --no-cache-dir
+pip3 install urllib3==1.26.2 --no-cache-dir
+pip3 install Werkzeug==1.0.1 --no-cache-dir
+pip3 install wrapt==1.12.1 --no-cache-dir
+pip3 install zipp==3.4.0 --no-cache-dir
+
+
+
+pip3 install jupyter
+jupyter notebook --generate-config
+ipython kernel install --user --name=algo
+jupyter notebook --ip=0.0.0.0 --port=5555 --allow-root
+
